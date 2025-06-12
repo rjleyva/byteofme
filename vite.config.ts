74f04bb7c8afb7cede903cebd4a2bb340223c162
RJ <rleyva@tutanota.com>
2025-06-12 06:23:03 -0400

    build(vite): set path alias for @ and set dev server port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
+import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      }
+    ]
+  },
+  server: {
+    port: 3000
+  }
 });
